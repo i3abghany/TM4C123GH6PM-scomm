336f6bf542fd29ed449ef73e6465aa9993a88ec8
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-09-04 22:28:18 +0200

    CAN: Added initial transmit message configuration.

--- CAN/CAN.c
+++ CAN/CAN.c
@@ -22,7 +22,7 @@ static void CAN_GPIO_init(enum CAN c) {
     }
 }
 
-static uint32_t get_canbit(uint32_t bitrate) {
+static uint32_t get_canbit(uint32_t bitrate, int8_t cfg_prop_time) {
     float bit_time = 1.0 / bitrate;
 
     /*
@@ -35,16 +35,21 @@ static uint32_t get_canbit(uint32_t bitrate) {
      * time_quantum = (BRP) / fsys 
      * BRP = time_quantum * fsys
      */
-    uint8_t prescaler = (uint32_t)(time_quantum * F_SYS_CLOCK) & (0b00111111);
+    uint8_t tBRP = ((uint32_t)(time_quantum * F_SYS_CLOCK) & (0xFF));
 
     /* tSync = 1 * time_quantum (fixed) */
     uint8_t sync_time = 1;
 
     /*
      * tProp depends on the physical characteristics of the CAN network.
-     * For the sake of simplicity, we'll fix it at 2 time quanta.
+     * For the sake of simplicity, we'll fix it at 2 time quanta with the
+     * ability to override it at configuration time.
      */
-    uint8_t prop_time = 2;
+    uint8_t prop_time = cfg_prop_time == 0 ? 2 : cfg_prop_time;
+
+    if (cfg_prop_time > N_TIME_QUANTA) {
+        return 0;
+    }
 
     /* tPhase1 = tPhase2 = 
      *     (N_TIME_QUANTA - ((tProp / time_quantum) + (tSync / time_quantum))
@@ -54,12 +59,11 @@ static uint32_t get_canbit(uint32_t bitrate) {
     uint8_t phase2_time = phases_time / 2;
     phase2_time += (phases_time % 2 != 0);
 
-    uint8_t tTSeg1 = (prop_time + phase1_time) & (0b00001111);
-    uint8_t tTSeg2 = (phase2_time) & (0b00000111);
+    uint8_t tTSeg1 = (prop_time + phase1_time);
+    uint8_t tTSeg2 = (phase2_time);
 
     /* The length of the synchronization jump width is set to the least of 4, Phase1 or Phase2 */
     uint8_t tSJW   = min(min(phase1_time, phase2_time), 4);
-    tSJW &= (0b00000011);
 
     /*
      * The four components TSEG2, TSEG1, SJW, and BRP have to be programmed to a
@@ -68,15 +72,26 @@ static uint32_t get_canbit(uint32_t bitrate) {
      * programmed. That way, for example, SJW (functional range of [1..4]) is
      * represented by only two bits in the SJW bit field.
      */
-    prescaler--;
+    tBRP--;
     tTSeg1--;
     tTSeg2--;
     tSJW--;
 
-    return (tTSeg2 << 12) | (tTSeg1 << 8) | (tSJW << 6) | (prescaler);
+    // return (tTSeg2 << 12) | (tTSeg1 << 8) | (tSJW << 6) | (prescaler);
+    return CONSTRUCT_CANBIT(tTSeg2, tTSeg1, tSJW, tBRP);
 }
 
-void CAN_init(struct CANConfig *cfg) {
+void CAN_disable(enum CAN c) {
+    uint32_t CANCTL = (c == CAN0) ? CAN0CTL : CAN1CTL;
+    PTR(CANCTL) |= CANCTL_INIT_MASK;
+}
+
+void CAN_enable(enum CAN c) {
+    uint32_t CANCTL = (c == CAN0) ? CAN0CTL : CAN1CTL;
+    PTR(CANCTL) &= ~CANCTL_INIT_MASK;
+}
+
+bool CAN_init(struct CANConfig *cfg) {
     CAN_GPIO_init(cfg->can_num);
     
     uint32_t CANCTL, CANBIT;
@@ -90,13 +105,116 @@ void CAN_init(struct CANConfig *cfg) {
     }
 
     /* Initialization is started by setting the INIT bit in the CANCTL register */
-    PTR(CANCTL) |= CANCTL_INIT_MASK;
+    CAN_disable(cfg->can_num);
 
     /* Set the CAN Bit Timing CANBIT register */
     PTR(CANCTL) |= CANCTL_CCE_MASK;
-    PTR(CANBIT) |= get_canbit(cfg->bit_rate);
+    uint32_t canbit = get_canbit(cfg->bit_rate, cfg->prop_time);
+
+    if (canbit == 0) {
+        return false;
+    }
+
+    PTR(CANBIT) |= canbit;
 
     /* To leave the initialization state, the INIT bit must be cleared. */
-    PTR(CANCTL) &= ~(CANCTL_INIT_MASK);
-    PTR(CANCTL) &= ~(CANCTL_CCE_MASK);
+    CAN_enable(cfg->can_num);
+
+    return true;
+}
+
+static void CAN_config_CANIF1CMSK(enum CAN c, uint32_t options) {
+    /* 1. In the CAN IFn Command Mask (CANIFnCMASK) register: */
+
+    uint32_t CANIF1CMSK;
+
+    if (c == CAN0) {
+        CANIF1CMSK = CAN0IF1CMSK;
+    } else {
+        CANIF1CMSK = CAN1IF1CMSK;
+    }
+
+    /* Set the WRNRD bit to specify a write to the CANIFnCMASK register; */
+
+    /*
+     * Conditionally set since it can alter that state of other bits. It's left
+     * for the CAN_config_CANIF1CMSK user to ensure consistent F1CMSK register
+     * state.
+     */
+    if (options & CANIF1CMSK_WRNRD_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_WRNRD_MASK;
+    }
+     
+    /* 
+     * specify whether to transfer the IDMASK, DIR, and MXTD of the message
+     * object into the CAN IFn registers using the MASK bit
+     */
+    if (options & CANIF1CMSK_MASK_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_MASK_MASK;
+    }
+
+    /*
+     * Specify whether to transfer the ID, DIR, XTD, and MSGVAL of the message
+     * object into the interface registers using the ARB bit
+     */
+    if (options & CANIF1CMSK_ARB_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_ARB_MASK;
+    }
+
+    /*
+     * Specify whether to transfer the control bits into the interface registers
+     * using the CONTROL bit
+     */
+    if (options & CANIF1CMSK_CONTROL_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_CONTROL_MASK;
+    }
+
+    /*
+     * Specify whether to clear the INTPND bit in the CANIFnMCTL register using
+     * the CLRINTPND bit
+     */
+    if (options & CANIF1CMSK_CLRINTPND_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_CLRINTPND_MASK;
+    }
+
+    /*
+     * Specify whether to clear the NEWDAT bit in the CANNWDAn register using
+     * the NEWDAT bit
+     */
+    if (options & CANIF1CMSK_NEWDAT_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_NEWDAT_MASK;
+    }
+
+    /*
+     * Specify which bits to transfer using the DATAA and DATAB bits
+     */
+    if (options & CANIF1CMSK_DATAA_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_DATAA_MASK;
+    }
+
+    if (options & CANIF1CMSK_DATAB_MASK) {
+        PTR(CANIF1CMSK) |= CANIF1CMSK_DATAB_MASK;
+    }
+}
+
+bool CAN_config_transmit_message(enum CAN c, struct CANMsgObject *msg)
+{
+    uint32_t CANIF1MSK1_value;
+    uint32_t CANIF1MSK2_value;
+
+    /* TODO: Should we allow the API user to specify those options? */
+    CAN_config_CANIF1CMSK(c, CANIF1CMSK_WRNRD_MASK |
+                             CANIF1CMSK_ARB_MASK   |
+                             CANIF1CMSK_CONTROL_MASK);
+    
+    bool use_29_bit_id = (msg->msg_id > CAN_11_BIT_ID_MASK);
+
+    if (use_29_bit_id) {
+        CANIF1MSK1_value = (msg->msg_id & CAN_11_BIT_ID_MASK);
+        CANIF1MSK2_value = (msg->msg_id >> 16);
+    } else {
+        CANIF1MSK1_value = 0;
+        CANIF1MSK2_value = ((msg->msg_id & CAN_11_BIT_ID_MASK) << 2);
+    }
+
 }
--- CAN/CAN.h
+++ CAN/CAN.h
@@ -1,7 +1,9 @@
 #ifndef _CAN_H
 #define _CAN_H
 
+#include <stdbool.h>
 #include "CANMap.h"
+
 #ifndef F_SYS_CLOCK
     #define F_SYS_CLOCK 25000000
 #endif
@@ -11,15 +13,32 @@ enum CAN {
     CAN1
 };
 
-enum CAN_MODE {
-    CAN_MASTER,
-    CAN_SLAVE
-};
-
 struct CANConfig {
     enum CAN can_num;
-    enum CAN_MODE mode;
     uint32_t bit_rate;
+    int8_t prop_time;
+};
+
+enum MsgObjectType {
+    CAN_MSG_OBJECT_TYPE_RX,
+    CAN_MSG_OBJECT_TYPE_RX_REMOTE,
+    CAN_MSG_OBJECT_TYPE_TX,
+    CAN_MSG_OBJECT_TYPE_TX_REMOTE
+};
+
+struct CANMsgObject {
+    /* Controller message object ID. */
+    uint32_t obj_id;
+
+    /* 11- or 29-bit message identifier (sent in the frame). */
+    uint32_t msg_id;
+
+    /* Used by the message handler in acceptance filtering */
+    uint32_t msg_id_mask;
+    uint32_t flags;
+    uint8_t data_len;
+    uint8_t *data;
+    enum MsgObjectType msg_type;
 };
 
 #define CAN0_RCGC0_OFFSET 24
@@ -37,10 +56,42 @@ struct CANConfig {
 #define CANCTL_INIT_MASK (1 << 0)
 #define CANCTL_CCE_MASK  (1 << 6)
 
-/* The bit time may consist of 4 to 25 time quanta.*/
+#define CANBIT_SEG2_POS 12
+#define CANBIT_SEG1_POS 8
+#define CANBIT_SJW_POS 6
+#define CANBIT_BRP_POS 0
+
+#define CANBIT_SEG2_MASK (0b00000111)
+#define CANBIT_SEG1_MASK (0b00001111)
+#define CANBIT_SJW_MASK  (0b00000011)
+#define CANBIT_BRP_MASK  (0b00111111)
+
+#define CANIF1CMSK_WRNRD_MASK     (0x00000080)
+#define CANIF1CMSK_MASK_MASK      (0x00000040)
+#define CANIF1CMSK_ARB_MASK       (0x00000020)
+#define CANIF1CMSK_CONTROL_MASK   (0x00000010)
+#define CANIF1CMSK_CLRINTPND_MASK (0x00000008)
+#define CANIF1CMSK_NEWDAT_MASK    (0x00000004)
+#define CANIF1CMSK_DATAA_MASK     (0x00000002)
+#define CANIF1CMSK_DATAB_MASK     (0x00000001)
+
+#define CONSTRUCT_CANBIT(seg2, seg1, sjw, brp)         \
+    (((seg2 & CANBIT_SEG2_MASK) << CANBIT_SEG2_POS) |  \
+    ((seg1 & CANBIT_SEG1_MASK) << CANBIT_SEG1_POS)  |  \
+    ((sjw  &  CANBIT_SJW_MASK) <<  CANBIT_SJW_POS)  |  \
+    ((brp  &  CANBIT_BRP_MASK) <<  CANBIT_BRP_POS))
+
+/* The bit time may consist of 4 to 25 time quanta. */
 #define N_TIME_QUANTA 5
+
+/*              Message Object Flags                */
+#define CAN_11_BIT_ID_MASK 0x000007FF
 
 #define min(x, y) ((x) < (y)) ? (x) : (y)
 
-void CAN_init(struct CANConfig *cfg);
+void CAN_disable(enum CAN c);
+void CAN_enable(enum CAN c);
+bool CAN_init(struct CANConfig *cfg);
+bool CAN_config_transmit_message(enum CAN, struct CANMsgObject *msg);
+
 #endif
