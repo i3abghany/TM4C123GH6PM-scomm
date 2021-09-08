3fea4d023a5a3b9c39fe8d2fb4f3d280f82ff4f2
Mahmoud Mandour <ma.mandourr@gmail.com>
2021-09-08 21:10:52 +0200

    CAN: Refactor message object config into functions.

--- CAN/main.c
+++ CAN/main.c
@@ -14,6 +14,28 @@ uint8_t receive_buffer[] = { '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
 CANMsgObject transmit_message_obj;
 CANMsgObject receive_message_obj;
 
+static inline void config_transmit_obj()
+{
+    transmit_message_obj.obj_id = g_transmit_obj_id;
+    transmit_message_obj.msg_id_mask = 0;
+    transmit_message_obj.msg_id = transmit_message_id;
+    transmit_message_obj.data_len = sizeof (transmit_data);
+    transmit_message_obj.data = transmit_data;
+    transmit_message_obj.flags = MSG_OBJ_TX_INT_ENABLED;
+    transmit_message_obj.msg_type = MSG_OBJ_TYPE_TX;
+}
+
+static inline void config_receive_obj()
+{
+    receive_message_obj.obj_id = g_receive_obj_id;
+    receive_message_obj.msg_id_mask = 0;
+    receive_message_obj.msg_id = receive_message_id;
+    receive_message_obj.data_len = 0;
+    receive_message_obj.data = receive_buffer;
+    receive_message_obj.flags = MSG_OBJ_RX_INT_ENABLED;
+    receive_message_obj.msg_type = MSG_OBJ_TYPE_RX;
+}
+
 int main()
 {
     g_errflag = 0;
@@ -26,24 +48,14 @@ int main()
     };
 
     CAN_init(&cfg);
+    /* TODO: Enable CAN0 interrupt in the NVIC. */
     CAN_interrupt_enable(CAN0, CAN_INTR_ALL);
+    CAN_enable(CAN0);
 
-    transmit_message_obj.obj_id = g_transmit_obj_id;
-    transmit_message_obj.msg_id_mask = 0;
-    transmit_message_obj.msg_id = transmit_message_id;
-    transmit_message_obj.data_len = sizeof (transmit_data);
-    transmit_message_obj.data = transmit_data;
-    transmit_message_obj.flags = MSG_OBJ_TX_INT_ENABLED;
-    transmit_message_obj.msg_type = MSG_OBJ_TYPE_TX;
+    config_transmit_obj();
     CAN_config_message(CAN0, &transmit_message_obj);
 
-    receive_message_obj.obj_id = g_receive_obj_id;
-    receive_message_obj.msg_id_mask = 0;
-    receive_message_obj.msg_id = receive_message_id;
-    receive_message_obj.data_len = 0;
-    receive_message_obj.data = receive_buffer;
-    receive_message_obj.flags = MSG_OBJ_RX_INT_ENABLED;
-    receive_message_obj.msg_type = MSG_OBJ_TYPE_RX;
+    config_receive_obj();
     CAN_config_message(CAN0, &receive_message_obj);
 
     while (1) {
